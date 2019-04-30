8e505f3b0bf0ddf5f310dff9b250a29896c4fab2
angelpray <1136472002@qq.com>
2019-04-30 15:10:23 +0800

    增加推荐和周末去哪儿组件

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -73,36 +73,39 @@ export default {
   .icons /deep/ .swiper-container {
     height: 50vw;
   }
-  .icon {
-    position: relative;
-    overflow: hidden;
-    float: left;
-    width: 25%;
-    padding-bottom: 25%;
-    .icon-img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: .44rem;
-      box-sizing: border-box;
-      padding: .1rem;
-      .icon-img-content {
-        display: block;
-        margin: 0 auto;
-        height: 100%;
+  .icons {
+    margin-top: .1rem;
+    .icon {
+      position: relative;
+      overflow: hidden;
+      float: left;
+      width: 25%;
+      padding-bottom: 25%;
+      .icon-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: .44rem;
+        box-sizing: border-box;
+        padding: .1rem;
+        .icon-img-content {
+          display: block;
+          margin: 0 auto;
+          height: 100%;
+        }
       }
-    }
-    .icon-desc {
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      height: .44rem;
-      line-height: .44rem;
-      color: @darkTextColor;
-      text-align: center;
-      .ellipsis();
+      .icon-desc {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: .44rem;
+        line-height: .44rem;
+        color: @darkTextColor;
+        text-align: center;
+        .ellipsis();
+      }
     }
   }
 </style>
